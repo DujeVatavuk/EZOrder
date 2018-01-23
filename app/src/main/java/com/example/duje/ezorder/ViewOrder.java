e408c7c6de72459ab4cbeb2184171babf020d8c1
Duje Vatavuk <duje@vatavuk.com>
2018-01-23 23:09:04 +0100

    izračun totalPrice

---
+++ app/src/main/java/com/example/duje/ezorder/ViewOrder.java
@@ -0,0 +1,8 @@
+package com.example.duje.ezorder;
+
+/**
+ * Created by duje on 23.1.2018..
+ */
+
+public class ViewOrder extends Order {
+}
---
+++ app/src/main/java/com/example/duje/ezorder/ViewOrderItem.java
@@ -0,0 +1,12 @@
+package com.example.duje.ezorder;
+
+/**
+ * Created by duje on 21.1.2018..
+ */
+
+public class OrderItem {
+    public int Id;
+    public int OrderId;
+    public int FoodId;
+    public int Quantity;
+}
