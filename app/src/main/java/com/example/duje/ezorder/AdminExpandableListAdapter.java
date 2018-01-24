37117fa6134b45c8e208263f53980efefcdccd20
Duje Vatavuk <duje@vatavuk.com>
2018-01-24 13:00:46 +0100

    Sve trazene funkcionalnosti gotove, doadano sve u AdminActivity , pozivanje baze i ispisivanje svih starih narudzbi

---
+++ app/src/main/java/com/example/duje/ezorder/AdminExpandableListAdapter.java
@@ -0,0 +1,10 @@
+package com.example.duje.ezorder;
+
+import android.widget.BaseExpandableListAdapter;
+
+/**
+ * Created by duje on 24.1.2018..
+ */
+
+public class AdminExpandableListAdapter extends BaseExpandableListAdapter {
+}
